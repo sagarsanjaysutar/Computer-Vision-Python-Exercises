5019a49ea63019e49b0f3cffcfafbf920510c30e
Sagar <sutarsagarsanjay@gmail.com>
2021-04-07 04:59:12 +0530

    Fixed path and increased max-iteration

--- ML/LBP.py
+++ ML/LBP.py
@@ -6,11 +6,12 @@ import cv2
 import os
 
 imageInfo = {
-    "training": "../Datasets/Training",
-    "testing": "../Datasets/Testing"
+    "training": "..\Datasets\Training",
+    "testing": "..\Datasets\Testing"
 }
 data = []
 labels = []
+
 
 def describe(numPoints, radius, image, eps=1e-7):
     lbp = feature.local_binary_pattern(image, numPoints, radius, 'uniform')
@@ -19,23 +20,24 @@ def describe(numPoints, radius, image, eps=1e-7):
     hist /= (hist.sum() + eps)
     return hist
 
+
 for imagePath in paths.list_images(imageInfo["training"]):
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     hist = describe(24, 8, gray)
-    # imagePath = Images / training / keyboard / keyboard_01.jpeg . Split it with "/" and we get - 2 elemet as the name i.e Folder name    
+    # imagePath = Images / training / keyboard / keyboard_01.jpeg . Split it with "/" and we get - 2 elemet as the name i.e Folder name
     labels.append(imagePath.split(os.path.sep)[-2])
     data.append(hist)
 
-model = LinearSVC(C=100.0, random_state=42)
+model = LinearSVC(C=100.0, random_state=42, max_iter=5000)
 model.fit(data, labels)
 
 for imagePath in paths.list_images(imageInfo["testing"]):
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     hist = describe(24, 8, gray)
-    predectition = model.predict(hist.reshape(1, -1)) #convert into 2d
-    cv2.putText(image, predectition[0], (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+    predectition = model.predict(hist.reshape(1, -1))  # convert into 2d
+    cv2.putText(image, predectition[0], (20, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1.0, (0, 0, 255), 3)
     cv2.imshow("LBP", image)
     cv2.waitKey(0)
